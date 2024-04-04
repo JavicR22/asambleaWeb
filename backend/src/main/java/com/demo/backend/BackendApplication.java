485528c6b6ed81fd7af357b32a07a20fb72e7311
Susosky <pipeprocampo@gmail.com>
2024-04-03 21:20:28 -0500

    AssemblyEntity and AttendanceEntity Models Creation

--- backend/src/main/java/com/demo/backend/BackendApplication.java
+++ backend/src/main/java/com/demo/backend/BackendApplication.java
@@ -1,19 +1,7 @@
 package com.demo.backend;
 
-import com.demo.backend.model.ERole;
-import com.demo.backend.model.PersonEntity;
-import com.demo.backend.model.RoleEntity;
-import com.demo.backend.model.UserEntity;
-import com.demo.backend.repositories.PersonRepository;
-import com.demo.backend.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Set;
 
 @SpringBootApplication
 public class BackendApplication {
--- backend/src/main/java/com/demo/backend/repositories/AttendanceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.backend.repositories;
-
-import com.demo.backend.model.AttendanceEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AttendanceRepository  extends CrudRepository<AttendanceEntity,Long>{
-}
