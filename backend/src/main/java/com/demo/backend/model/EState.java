1c0b9d14651eb8f2022b29a642d844f847a7932d
JavicR22 <101529906+JavicR22@users.noreply.github.com>
2024-04-08 22:43:36 -0500

    Actualización del back y front

---
+++ backend/src/main/java/com/demo/backend/model/EState.java
@@ -0,0 +1,2 @@
+package com.demo.backend.model;public enum EState {
+}
--- backend/src/main/java/com/demo/backend/repositories/AttendanceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.backend.repositories;
-
-import com.demo.backend.model.AttendanceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendanceRepository  extends JpaRepository<AttendanceEntity,Long> {
-}
